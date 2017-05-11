60798044747d9ff61a8aafc2b43850e4b1f50903
Taehee Lee <kairess87@gmail.com>
2017-05-11 13:56:23 +0900

    initial commit8

--- ImageProc.h
+++ ImageProc.h
@@ -43,15 +43,15 @@ struct buffer {
 
 static char            dev_name[16];
 static int              fd              = -1;
-struct buffer *         buffers         = NULL;
+// struct buffer *         buffers         = NULL;
 static unsigned int     n_buffers       = 0;
 
-int camerabase = -1;
+// int camerabase = -1;
 
-int *rgb = NULL;
-int *ybuf = NULL;
+// int *rgb = NULL;
+// int *ybuf = NULL;
 
-int yuv_tbl_ready=0;
+// int yuv_tbl_ready=0;
 int y1192_tbl[256];
 int v1634_tbl[256];
 int v833_tbl[256];
